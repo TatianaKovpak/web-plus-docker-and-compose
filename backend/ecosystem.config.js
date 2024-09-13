146f95e32b5e013b08d4b78a2c4b1ee2b8c6e2f7
unknown <tanechka.kovpak@yandex.ru>
2024-09-13 14:52:58 +0300

    2

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,6 +1,12 @@
-module.exports = [
-  {
-    script: 'dist/main.js',
-    name: 'web-plus-docker-and-compose',
-  },
-];
+module.exports = {
+  apps: [
+    {
+      name: 'web-plus-docker-and-compose',
+      script: 'dist/main.js',
+      watch: false,
+      restart_delay: 1000,
+      max_restarts: 10,
+      autorestart: true,
+    },
+  ],
+};
